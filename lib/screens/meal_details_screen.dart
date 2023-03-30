7ef7d4b0f7910a955704331568c943837791e373
kartikpachori <kartikeypachori1610@gmail.com>
2023-03-30 23:34:31 +0530

    styling

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   static const routename = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('$mealId'),
-      ),
-      body: Center(
-        child: Text('THE MEAL -$mealId!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('${selectedMeal.title}'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            )
+          ],
+        ));
   }
 }
