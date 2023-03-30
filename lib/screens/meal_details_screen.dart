a9970a3ced164c37c3aa174407fc3a6d271d2689
kartikpachori <kartikeypachori1610@gmail.com>
2023-03-30 23:50:17 +0530

    STEPS OF ingredients

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -27,7 +27,25 @@ class MealDetailsScreen extends StatelessWidget {
                 'Ingredients',
                 style: Theme.of(context).textTheme.headline6,
               ),
-            )
+            ),
+            Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 150,
+                width: 300,
+                child: ListView.builder(
+                  itemBuilder: ((ctx, index) => Card(
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            child: Text(selectedMeal.ingredients[index])),
+                      )),
+                  itemCount: selectedMeal.ingredients.length,
+                ))
           ],
         ));
   }
